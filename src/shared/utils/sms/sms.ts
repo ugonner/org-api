c05c87fc6a99dd90fb68643cdbb6ecffc6c1113a
ugonner <umeokwonnabonaventure@gmail.com>
2024-05-16 21:49:43 +0100

    sends out sms with infobip

--- src/shared/utils/sms/sms.ts
+++ src/shared/utils/sms/sms.ts
@@ -1,51 +1,58 @@
-import { Body, HttpStatus } from "@nestjs/common";
-import { ApiResponse, IGenericResponse } from "../../apiResponse";
+import { Body, HttpStatus } from '@nestjs/common';
+import { ApiResponse, IGenericResponse } from '../../apiResponse';
 
 export interface IInfoBipDestination {
-    to: string;
+  to: string;
 }
 export interface IInfoBipSMSMessage {
-    destinations: IInfoBipDestination[];
-    from: string;
-    text: string;
+  destinations: IInfoBipDestination[];
+  from: string;
+  text: string;
 }
 export interface IInfoBipSMSPayload {
-    messages: IInfoBipSMSMessage[];
+  messages: IInfoBipSMSMessage[];
 }
 
 export class SMSService {
+  static async sendSMSWithInfoBip(
+    destinationNumbers: string[],
+    messageText: string,
+  ): Promise<IGenericResponse<boolean>> {
+    try {
 
-    static async sendSMSWithInfoBip(destinationNumbers: string[], messageText: string): Promise<IGenericResponse<boolean>>{
-        try{
+      const destinations: IInfoBipDestination[] = destinationNumbers.map((num) => {
+        const formattedNum = num.substr(0, 3) !== "234" || num.length !== 13 ? `234${num}` : num;
+        return {to: formattedNum}
+        
+      });
+      const payload: IInfoBipSMSPayload = {
+        messages: [
+          {
+            destinations,
+            text: messageText,
+            from: `${process.env.INFOBIP_SMS_SERVICE_NAME}`,
+          },
+        ],
+      };
 
-            const payload: IInfoBipSMSPayload = {
-                messages: [
-                    {
-                        destinations: destinationNumbers.map((num) => ({to: num})),
-                        text: messageText,
-                        from: `${process.env.INFOBIP_SMS_SERVICE_NAME}`
-                    }
-                ]
-            }
-            
-            
-            fetch(`${process.env.INFOBIP_BASE_URL}`,
-            {
-
-                method: "POST",
-                headers: {
-                    "Authorization": `${process.env.INFOBIP_TOKEN}`,
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                },
-                body: JSON.stringify(payload),
-            }
-        )
-        .then(() => console.log("sms sent"))
-        .catch((err) => console.log("Error sending sms", err.message))
-            return ApiResponse.success("sms sent", HttpStatus.OK, true)
-        }catch(error){
-            return ApiResponse.fail(error.message, HttpStatus.INTERNAL_SERVER_ERROR, error);
-        }
+      fetch(`${process.env.INFOBIP_BASE_URL}`, {
+        method: 'POST',
+        headers: {
+          Authorization: `${process.env.INFOBIP_TOKEN}`,
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+        .then(() => console.log('sms sent'))
+        .catch((err) => console.log('Error sending sms', err.message));
+      return ApiResponse.success('sms sent', HttpStatus.OK, true);
+    } catch (error) {
+      return ApiResponse.fail(
+        error.message,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        error,
+      );
     }
+  }
 }
